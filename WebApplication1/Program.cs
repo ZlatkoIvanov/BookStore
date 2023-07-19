22ca5e7b03a23d887062178a36ad2ccf0134e13c
ZlatkoIvanov <125381651+ZlatkoIvanov@users.noreply.github.com>
2023-07-19 09:25:47 +0300

    change

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -8,6 +8,7 @@ namespace WebApplication1
 
             // Add services to the container.
 
+
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
